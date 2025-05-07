7fe82e1f22f6ba54fd926b92d323672fc412eeaf
triyank-pedalsup <triyank.bhavaliya@pedalsup.com>
2025-05-07 14:13:48 +0530

    render fix bind method

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -19,9 +19,9 @@ export class UserRoutes{
     }
 
     inRoutes(): void{
-        this.router.post("/register",this.userController.register)
-        this.router.post("/login",this.userController.login)
-        this.router.post("/admin",this.jwtMiddleware.verifyToken,this.jwtMiddleware.checkRole('admin'),this.userController.adminLogin)
-        this.router.post("/user",this.jwtMiddleware.verifyToken,this.jwtMiddleware.checkRole('user'),this.userController.userLogin)
+        this.router.post("/register",this.userController.register.bind(this.userController))
+        this.router.post("/login",this.userController.login.bind(this.userController))
+        this.router.post("/admin",this.jwtMiddleware.verifyToken,this.jwtMiddleware.checkRole('admin'),this.userController.adminLogin.bind(this.userController))
+        this.router.post("/user",this.jwtMiddleware.verifyToken,this.jwtMiddleware.checkRole('user'),this.userController.userLogin.bind(this.userController))
     }
 }
