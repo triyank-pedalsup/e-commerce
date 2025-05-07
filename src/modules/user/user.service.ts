e718bdebc68374dc5585ad2b756bd1816651eba8
triyank-pedalsup <triyank.bhavaliya@pedalsup.com>
2025-05-07 15:28:47 +0530

    refactor: Update service methods to use specific DTO types instead of 'any'

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,17 +1,17 @@
 import { PrismaClient } from '../../../prisma/generated/prisma/index.js';
-// import { LoginUserDto } from './dto/login.dto.js';
-// import { RegisterUserDto } from './dto/register.dto.js';
+import { LoginUserDto } from './dto/login.dto.js';
+import { RegisterUserDto } from './dto/register.dto.js';
 const prisma = new PrismaClient();
 
 export class UserService {
-    public async register(data: any){
+    public async register(data: RegisterUserDto){
         const user = await prisma.user.create({
             data
         })
         return user;
     }
 
-    public async login(email: string){
+    public async login({email}: Pick<LoginUserDto, 'email'>){
         const user = await prisma.user.findUnique({
             where:{
                 email
