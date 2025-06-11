50c71aced4d9fd5fce6b63e905361488e685f52f
triyank-pu <triyank.bhavaliya@pedalsup.com>
2025-06-11 23:57:28 +0530

    Update inject-cls.ts

--- src/decorator/inject-cls.ts
+++ src/decorator/inject-cls.ts
@@ -1,3 +1,5 @@
+//this is the simple example of the dacorator and how to use the decorator
+
 export function InjectCls<T>(cls: { new(): T}): PropertyDecorator{
     return (target: any, propertyKey: string | symbol) => {
         target[propertyKey] = new cls();
